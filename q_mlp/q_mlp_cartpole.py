Updated dqn to q_mlp. Fixed q_value computation.

q_mlp/q_mlp_cartpole.py
@@ -11,8 +11,6 @@ import gym
 import tensorflow as tf
 from tensorflow import keras
 
-#TODO: Change to episodic learning instead of long run
-
 # Reward decay
 GAMMA = 0.9
 # Learning Rate
@@ -28,7 +26,7 @@ EPSILON_MAX = 1
 EPSILON_DECAY = 0.9
 
 # Number Of Episodes to run
-EPISODES = 5000
+EPISODES = 1000
 
 class DQNAgent:
     """ """
@@ -73,7 +71,7 @@ class DQNAgent:
         :param state: 
 
         """
-        if np.random.rand() < self.epsilon:
+        if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_space)
         q_values = self.q_net.predict(state)
         return np.argmax(q_values[0])
@@ -85,12 +83,13 @@ class DQNAgent:
         batch = random.sample(self.memory, BATCH_SIZE)
         for state, action, reward, next_state, done in batch:
             q_update = reward
+            print("q_update before discount:{}".format(q_update))
             if not done:
-                q_update = reward
-                + GAMMA * np.amax(self.q_net.predict(next_state)[0])
+                q_update = reward + GAMMA * np.amax(self.q_net.predict(next_state)[0])
+                print("q_update after discount:{}".format(q_update))
             q_values = self.q_net.predict(state)
             q_values[0][action] = q_update
-            # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs", update_freq='batch')
+            # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs", histogram_freq=0, write_graph=True, write_images=True)
             self.q_net.fit(state, q_values, verbose=0)
         self.epsilon *= EPSILON_DECAY
         self.epsilon = max(EPSILON_MIN, self.epsilon)
@@ -109,10 +108,11 @@ def main():
         done = False
         while not done:
             step += 1
-            # env.render()
+            env.render()
             action = dqn_agent.act(state)
             next_state, reward, done, info = env.step(action)
             reward = reward if not done else -reward
+            print(reward)
             next_state = np.reshape(next_state,
                                     [1, env.observation_space.shape[0]])
             dqn_agent.memorize(state, action, reward, next_state, done)