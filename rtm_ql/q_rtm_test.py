143951578860af071aa320a959c132d1ace4681c
Varun Ravi Varma <varunravivarma@gmail.com>
2020-08-19 15:08:14 +0200

    Added stretched exponential epsilon decay function

--- rtm_ql/q_rtm_test.py
+++ rtm_ql/q_rtm_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import math
 from collections import deque
 import gym
 from logger.score import ScoreLogger
@@ -16,7 +17,7 @@ BATCH_SIZE = 25
 # Exploration-Exploitation Parameters
 EPSILON_MIN = 0.01
 EPSILON_MAX = 1.0
-EPSILON_DECAY = 0.5
+EPSILON_DECAY = 0.9
 
 # Number Of Episodes to run
 EPISODES = 300
@@ -35,6 +36,19 @@ class RTMQL:
         self.agent_1 = self.tm_model()
         self.agent_2 = self.tm_model()
 
+    def linear_eps_decay(self, eps_decay=EPSILON_DECAY):
+        self.epsilon = self.epsilon * eps_decay
+        return max(EPSILON_MIN, self.epsilon)
+
+    def stretched_exp_eps_decay(self, current_ep):
+        A = 0.1
+        B = 0.3
+        C = 0.1
+        standardized_time = (current_ep-A*EPISODES)/(B*EPISODES)
+        cosh = np.cosh(math.exp(-standardized_time))
+        self.epsilon = 1.1-(1/cosh+(current_ep*C/EPISODES))
+        return self.epsilon
+
     def tm_model(self):
         self.tm_agent = QRegressionTsetlinMachine(number_of_clauses=100, T=100*8, s=2.75, reward=1, gamma=GAMMA, max_score=200, number_of_actions=2)
         self.tm_agent.number_of_patches = 2
@@ -50,13 +64,14 @@ class RTMQL:
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_space)
         q_values = [self.agent_1.predict(state), self.agent_2.predict(state)]
-        print("Q Value: {}".format(q_values))
+        # print("Q Value: {}".format(q_values))
         return np.argmax(q_values)
 
-    def experience_replay(self):
-        self.epsilon = EPSILON_MAX
+    def experience_replay(self, episode):
+        # self.epsilon = EPSILON_MAX
         if len(self.memory) < BATCH_SIZE:
             return
+        batch_ctr = 0
         batch = random.sample(self.memory, BATCH_SIZE)
         for state, action, reward, next_state, done in batch:
             q_update = reward
@@ -68,9 +83,13 @@ class RTMQL:
             q_values[action] = q_update
             self.agent_1.fit(state, q_values[0])
             self.agent_2.fit(state, q_values[1])
+        batch_ctr += 1
+        print("Batch_Ctr: {}".format(batch_ctr))
         print("Epsilon: {}".format(self.epsilon))
-        self.epsilon *= EPSILON_DECAY
-        self.epsilon = max(EPSILON_MIN, self.epsilon)
+        # LINEAR EPSILON DECAY
+        # self.epsilon = self.linear_eps_decay()
+        # STRETCHED EXPONENTIAL EPSILON DECAY
+        self.epsilon = self.stretched_exp_eps_decay(episode)
 
 
 
@@ -80,8 +99,7 @@ def main():
     discretizer = CustomDiscretizer()
     rtm_agent = RTMQL(env)
     prev_actions = []
-    total_iter = 0
-    for ep in range(EPISODES):
+    for curr_ep in range(EPISODES):
         # print("EPISODE NUMBER: {}".format(ep))
         state = env.reset()
         state = discretizer.cartpole_discretizer(input_state=state)
@@ -90,7 +108,6 @@ def main():
         done = False
         while not done:
             step += 1
-            total_iter += 1
             # env.render()
             action = rtm_agent.act(state)
             # print(action)
@@ -105,11 +122,10 @@ def main():
             state = next_state
             if done:
                 print("Episode: {0}\nEpsilon: {1}\tScore: {2}".format(
-                ep, rtm_agent.epsilon, step))
-                score_log.add_score(step, ep, GAMMA, EPSILON_DECAY)
+                curr_ep, rtm_agent.epsilon, step))
+                score_log.add_score(step, curr_ep, GAMMA, EPSILON_DECAY)
                 break
-            rtm_agent.experience_replay()
-    # print("TOTAL_ITER: {}".format(total_iter))
+            rtm_agent.experience_replay(curr_ep)
 
 
 if __name__ == "__main__":
